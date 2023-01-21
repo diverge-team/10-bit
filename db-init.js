bf1726569d790a1552b26b0cbcf5a62ff2413638
Florian "Ori" Neveu <hello@fnev.eu>
2023-01-21 03:56:05 +0100

    [test] Rename database env params and fix tests

--- db-init.js
+++ db-init.js
@@ -1,7 +1,7 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize(process.env.SQLITE_DATABASE, process.env.SQLITE_USER, process.env.SQLITE_PASSWORD, {
-    host: process.env.SQLITE_HOST,
+const sequelize = new Sequelize(process.env.DB_DATABASE, process.env.DB_USER, process.env.DB_PASSWORD, {
+    host: process.env.DB_HOST,
     dialect: 'mariadb',
     logging: false,
 });
