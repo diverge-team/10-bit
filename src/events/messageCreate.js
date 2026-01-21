09c60f91092f21c042834ab5693456c76a33f677
Florian "Ori" Neveu <hello@fnev.eu>
2026-01-21 19:13:19 +0100

    Alyxal <3

--- src/events/messageCreate.js
+++ src/events/messageCreate.js
@@ -9,6 +9,14 @@ const POUR_FEUR_VARIANTS = [
     'pour feur', 'Pour feur.', 'Pour feur ?', 'POUR FEUR !', 'pour feur...', 'Pour feur 🌺',
 ];
 
+const NATA_GIFS = [
+    'https://media0.giphy.com/media/v1.Y2lkPTc5MGI3NjExaDA2OHRhOXQzY2ppMTNpZnk2dXN4NXdkNjZzaWlvbnBhbTV2YnNwdCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/zUml4ECfPmpv2Rvw8C/giphy.gif',
+    'https://media2.giphy.com/media/v1.Y2lkPTc5MGI3NjExcWNseHI2dmptNXVvcmRoaWFzNTd0eDd6cHR4b2RoMXhudm83ajY1ZiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/91BKhwDl3bYyOfEviA/giphy.gif',
+    'https://media.giphy.com/media/v1.Y2lkPWVjZjA1ZTQ3bWZ3b3dhMzR6am44bHNhY3JhbXdqb2t4ODN4bnkxbGdtc2R1YWV4ayZlcD12MV9naWZzX3JlbGF0ZWQmY3Q9Zw/85IehmL7hzfdi5t38v/giphy.gif',
+    'https://media.giphy.com/media/v1.Y2lkPWVjZjA1ZTQ3bWZ3b3dhMzR6am44bHNhY3JhbXdqb2t4ODN4bnkxbGdtc2R1YWV4ayZlcD12MV9naWZzX3JlbGF0ZWQmY3Q9Zw/C7NPVfbAMdbYs1cTZi/giphy.gif',
+    'https://media.giphy.com/media/v1.Y2lkPWVjZjA1ZTQ3b3IxZDdkbTJibGVldW9pMGFuaHU2bzN2bmVkc2RnMHJyczBvYTJwcCZlcD12MV9naWZzX3JlbGF0ZWQmY3Q9Zw/mAbQSUCdXldtQp1n19/giphy.gif',
+];
+
 function pickRandom(array) {
     return array[Math.floor(Math.random() * array.length)];
 }
@@ -57,6 +65,12 @@ module.exports = {
         // Les lolos (2% chance)
         if (Math.random() < 0.02) {
             await message.channel.send('Les lolos.');
+        }
+
+        // Alyxal response (5% chance)
+        if (message.author.id === process.env.ALYXAL_USER_ID && Math.random() < 0.05) {
+            const gif = pickRandom(NATA_GIFS);
+            await message.reply(`Tu as toujours l'air occupée, Chasseuse...\n${gif}`);
         }
     },
 };
